8b5be32267c0c5ad7ffdc2e797761e9aabf1b92e
Fabio Jose Muneratti Ortega <fabiojmortega@yahoo.com.br>
2024-09-18 16:31:46 -0300

    Initial version.

---
+++ README.md
@@ -0,0 +1,37 @@
+# Autochat: a tool for automating LLM tests.
+
+Autochat is a CLI tool for simplifying the task of collecting multiple responses from a chatbot.
+
+## Installation
+
+Clone the repository. That's about it.
+
+The repository includes a bash script to create, activate and populate a Python virtual environment for the sake of running the tool. If you choose to use it, simply add execution permissions to `autochat` and use that script to invoke the tool. If not, install the dependencies from the `requirements.txt` file (`pip install -r requirements.txt` should suffice) and use the python script `autochat.py` directly.
+
+## Usage
+
+Create a text file containing the prompts to send sequentially to the LLM. Each message must be separated from the previous one by two blank lines. The `sample_inputs` directory contains examples. Assuming a local Ollama installation in the standard port:
+
+```bash
+./autochat --url http://localhost:11434/api/chat --model mistral sample_inputs/three_questions.txt
+```
+
+Or, using the Python script directly:
+
+```bash
+python autochat.py --url http://localhost:11434/api/chat --model mistral sample_inputs/three.questions.txt
+```
+
+To simplify usage, you may also set the URL in an environment variable `LLM_API_URL` instead.
+
+## Other Features
+
+- Custom API parameters via the `--raw` option
+- Independent messages or conversations with history
+
+See the help for details.
+
+```bash
+./autochat --help
+```
+
---
+++ autochat.py
@@ -0,0 +1,103 @@
+#!/bin/env python
+
+import requests
+import json
+import sys
+import argparse
+import logging
+import os
+
+logging.basicConfig(level=logging.INFO)
+
+def send_messages(args):
+
+    url = args.url
+    url = os.getenv('LLM_API_URL', default=None) if url is None else url
+
+    if url is None:
+        print("LLM_API_URL variable is not set.")
+        return 1
+
+    # Load the text file and split into paragraphs (separated by two empty lines)
+    with open(args.input_file, 'r') as file:
+        content = file.read()
+        messages = content.split('\n\n\n')  # Assuming two empty lines between paragraphs
+
+    session = requests.Session()  # Persist connection
+    thread_msgs = []
+
+    output = sys.stdout
+    if args.output is not None:
+        output = open(args.output, 'a')
+
+    for i, message in enumerate(messages):
+        print(f"Sending message {i+1}...\n")
+        if (args.raw):
+            payload = json.loads(message.strip())
+        else:
+            payload = {
+                    "model": args.model,
+                    "stream": False
+                    }
+            
+            new_msg = {
+                    "role": "user",
+                    "content": message.strip()
+                    }
+
+            thread_msgs.append(new_msg)
+            payload['messages'] = thread_msgs if args.chat else [new_msg]
+            
+        # Send POST request
+        response = session.post(url, json=payload, headers={"Content-Type":"application/json"})
+
+        # Check for successful response
+        if response.status_code == 200:
+            logging.debug(response.text)
+            try:
+                response_data = response.json()  # Assuming the response is in JSON format
+            except json.decoder.JSONDecodeError as e:
+                print(e)
+                print(response.text)
+                return 1
+            thread_msgs.append(response_data['message'])
+            print(f"Response {i+1}:\n------------\n")
+            if (args.full_response):
+                print(f"{json.dumps(response_data, indent=2)}\n", file=output)
+            else:
+                print(f"{response_data['message']['content']}\n", file=output)
+
+        else:
+            print(f"Error with message {i+1}: Status code {response.status_code}")
+            print(response.text)
+            output.close()
+            return 1
+    
+    output.close()
+    return 0
+
+if __name__ == "__main__":
+    sys.stdout.reconfigure(encoding='utf-8')
+    parser = argparse.ArgumentParser(
+        prog='autochat',
+        description='Automates the passing of messages to an LLM API.',
+        formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('--url', type=str,
+                        help='Endpoint URL')
+    parser.add_argument('input_file', metavar='I', type=str,
+                        help='Text file containing double-line-break separated messages.')
+    parser.add_argument('--raw', action='store_true',
+                        help='Send messages as-is. Requires JSON formatted messages.')
+    parser.add_argument('--model', type=str, default='llama3.1',
+                        help='The model name.')
+    parser.add_argument('--chat', action='store_true',
+                        help='Send previous messages as chat history.')
+    parser.add_argument('--full-response', action='store_true',
+                        help='Output the full API response, in JSON.')
+    parser.add_argument('-o', '--output', type=str,
+                        help='Path to output file.')
+    args = parser.parse_args()
+
+    exit_code = send_messages(args)
+    sys.exit(exit_code)
+
